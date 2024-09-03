91822b737281f9b02a32188cdf1df8d6b2d67c15
Adi Pratama Putra <147826682+pratamaputraadi@users.noreply.github.com>
2024-09-03 19:25:18 +0700

    Pertemuan-1

---
+++ pertemuan1/MahasiswaBeraksi.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author Adi Pratama Putra
+ */
+public class MahasiswaBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa fasilkomif = new Mahasiswa();
+        
+        fasilkomif.membaca();
+        fasilkomif.nyontek();
+        fasilkomif.modifikasi();
+    }
+    
+}
