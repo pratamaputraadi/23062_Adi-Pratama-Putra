ade36169d48a39a913bc9d1b461617ec2aeaafa4
Adi Pratama Putra <147826682+pratamaputraadi@users.noreply.github.com>
2024-09-06 21:43:26 +0700

    Pertemuan-2

---
+++ 23062_Adi-Pratama-Putra_Praktikum_Tugas2/Pertemuan_2/Nomor_1/MatematikaBeraksi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pertemuan_2.Nomor_1;
+
+/**
+ * LATIHAN NOMOR 1
+ * PERTEMUAN 2 - PEMROGRAMAN BERORIENTASI OBJEK
+ * @author Adi Pratama Putra
+ */
+
+public class MatematikaBeraksi {
+
+    public static void main(String[] args) {
+        Matematika hitung = new Matematika();
+        
+        hitung.pertambahan(20, 10);
+        hitung.pengurangan(10, 5);
+        hitung.perkalian(10, 3);
+        hitung.pembagian(21, 2);
+    }
+    
+}
---
+++ 23062_Adi-Pratama-Putra_Praktikum_Tugas2/Pertemuan_2/Nomor_2/InterfaceMatematika.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Pertemuan_2.Nomor_2;
+
+/**
+ * LATIHAN NOMOR 2
+ * PERTEMUAN 2 - PEMROGRAMAN BERORIENTASI OBJEK
+ * @author Adi Pratama Putra
+ */
+public interface InterfaceMatematika {
+    public void pertambahan( int a, int b);
+    public void pengurangan( int a, int b);
+    public void perkalian( int a, int b);
+    public void pembagian( int a, int b);
+}
