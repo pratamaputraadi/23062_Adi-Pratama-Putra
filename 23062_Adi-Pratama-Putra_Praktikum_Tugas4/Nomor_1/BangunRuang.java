bc48f24c22dd1c68b596ee37f27842e5496e92d5
Adi Pratama Putra <147826682+pratamaputraadi@users.noreply.github.com>
2024-09-23 19:43:17 +0700

    Pertemuan-4

---
+++ 23062_Adi-Pratama-Putra_Praktikum_Tugas4/Nomor_1/BangunRuang.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan_4.Nomor_1;
+
+/**
+ * LATIHAN NOMOR 1
+ * PERTEMUAN 4 - PEMROGRAMAN BERORIENTASI OBJEK
+ * @author Adi Pratama Putra
+ */
+
+public class BangunRuang {
+    int volume(int panjang, int lebar, int tinggi){
+    return panjang * lebar * tinggi ;
+    }
+    
+    int volume(int sisi){
+    return sisi * sisi * sisi ;
+    }
+    
+    int luasPermukaan(int panjang, int lebar, int tinggi){
+    return 2 * ( panjang * lebar + panjang * tinggi + lebar * tinggi);
+    }
+    
+    int luasPermukaan(int sisi){
+    return 6 * ( sisi * sisi );
+    }
+    
+}
+// TERIMA KASIH - THANK YOU//
---
+++ 23062_Adi-Pratama-Putra_Praktikum_Tugas4/Nomor_1/BangunRuangBeraksi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pertemuan_4.Nomor_1;
+
+/**
+ * LATIHAN NOMOR 1
+ * PERTEMUAN 1 - PEMROGRAMAN BERORIENTASI OBJEK
+ * @author Adi Pratama Putra
+ */
+
+public class BangunRuangBeraksi {
+    public static void main(String[] args) {
+        BangunRuang operasi = new BangunRuang();
+        
+        System.out.println("\nDIKETAHUI BALOK MEMILIKI");
+        System.out.println("- Panjang = 10 cm \n- Lebar   = 15 cm \n- Tinggi  = 20 cm");
+        System.out.println("VOLUME BALOK ------- = " + operasi.volume(10, 15, 20) + " cm");
+        System.out.println("LUAS PERMUKAAN BALOK = " + operasi.luasPermukaan(10, 15, 20) + " cm");
+        
+        System.out.println("\n\nDIKETAHUI KUBUS MEMILIKI");
+        System.out.println("- Sisi    = 15 cm");
+        System.out.println("VOLUME KUBUS ------- = " + operasi.volume (15) + " cm");
+        System.out.println("LUAS PERMUKAAN KUBUS = " + operasi.luasPermukaan (15) + " cm");
+        
+        System.out.println("");
+        
+    }
+    
+}
