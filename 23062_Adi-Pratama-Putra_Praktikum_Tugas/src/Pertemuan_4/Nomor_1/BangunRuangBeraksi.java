ddef29544f1d8b02650648c7cc29bcedd29cfd8b
Adi Pratama Putra <147826682+pratamaputraadi@users.noreply.github.com>
2024-09-24 20:55:04 +0700

    Pemrograman Berorientasi Objek

--- 23062_Adi-Pratama-Putra_Praktikum_Tugas/src/Pertemuan_4/Nomor_1/BangunRuangBeraksi.java
+++ 23062_Adi-Pratama-Putra_Praktikum_Tugas/src/Pertemuan_4/Nomor_1/BangunRuangBeraksi.java
@@ -12,20 +12,20 @@ package Pertemuan_4.Nomor_1;
 
 public class BangunRuangBeraksi {
     public static void main(String[] args) {
-        BangunRuang operasi = new BangunRuang();
+        BangunRuang operasi = new BangunRuang(); // MEMBUAT OBJEK DARI KELAS BANGUN RUANG
         
         System.out.println("\nDIKETAHUI BALOK MEMILIKI");
         System.out.println("- Panjang = 10 cm \n- Lebar   = 15 cm \n- Tinggi  = 20 cm");
-        System.out.println("VOLUME BALOK ------- = " + operasi.volume(10, 15, 20) + " cm");
-        System.out.println("LUAS PERMUKAAN BALOK = " + operasi.luasPermukaan(10, 15, 20) + " cm");
+        System.out.println("VOLUME BALOK ------- = " + operasi.volume(10, 15, 20) + " cm"); // MENGHITUNG DAN MENAMPILKAN VOLUME BALOK
+        System.out.println("LUAS PERMUKAAN BALOK = " + operasi.luasPermukaan(10, 15, 20) + " cm"); // MENGHITUNG DAN MENAMPILKAN LUAS PERMUKAAN BALOK
         
         System.out.println("\n\nDIKETAHUI KUBUS MEMILIKI");
         System.out.println("- Sisi    = 15 cm");
-        System.out.println("VOLUME KUBUS ------- = " + operasi.volume (15) + " cm");
-        System.out.println("LUAS PERMUKAAN KUBUS = " + operasi.luasPermukaan (15) + " cm");
+        System.out.println("VOLUME KUBUS ------- = " + operasi.volume (15) + " cm"); // MENGHITUNG DAN MENAMPILKAN VOLUME KUBUS
+        System.out.println("LUAS PERMUKAAN KUBUS = " + operasi.luasPermukaan (15) + " cm"); // MENGHITUNG DAN MENAMPILKAN LUAS PERMUKAAN KUBUS
         
         System.out.println("");
         
-    }
-    
+    }   
 }
+// TERIMA KASIH - THANK YOU //
