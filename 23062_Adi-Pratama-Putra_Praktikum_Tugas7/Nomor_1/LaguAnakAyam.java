18f479344a8297bd5684275df8f28cde14375803
Adi Pratama Putra <147826682+pratamaputraadi@users.noreply.github.com>
2024-10-15 01:28:55 +0700

    Pertemuan-7

---
+++ 23062_Adi-Pratama-Putra_Praktikum_Tugas7/Nomor_1/LaguAnakAyam.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pertemuan_7.Nomor_1;
+
+/**
+ * PERTEMUAN 7 - PEMROGRAMAN BERORIENTASI OBJEK
+ * LATIHAN NOMOR 1
+ * @author Adi Pratama Putra
+ */
+
+public class LaguAnakAyam {
+     public static void main(String[] args) {
+        
+        // LIRIK AWAL LAGU ANAK AYAM
+        System.out.println("TEK-KOTEK-KOTEK-KOTEK");
+        System.out.println("ANAK AYAM TURUN BERKOTEK");
+        System.out.println("TEK-KOTEK-KOTEK-KOTEK");
+        System.out.println("ANAK AYAM TURUN BERKOTEK");
+        System.out.println(" ");
+        
+        
+        // LIRIK LAGU ANAK AYAM 6 - 1
+        for (int i = 6; i >= 1; i--) {
+            // JUMLAH ANAK AYAM YANG TURUN
+            System.out.println("ANAK AYAM TURUNLAH " + i);
+            if (i > 1) {
+                
+                // JIKA LEBIH DARI 1 MAKAN AKAN MENAMPILKAN :
+                System.out.println("PERGI SATU, TINGGALLAH " + (i - 1));
+            if (i == 5 || i == 3) {
+                System.out.println("\nTEK-KOTEK-KOTEK-KOTEK");
+                System.out.println("ANAK AYAM TURUN BERKOTEK");
+                System.out.println("TEK-KOTEK-KOTEK-KOTEK");
+                System.out.println("ANAK AYAM TURUN BERKOTEK");
+                System.out.println(" ");
+            
+                
+            } 
+        }
+        }
+
+        // LIRIK AKHIR LAGU ANAK AYAM
+        System.out.println("PERGI SATU TINGGAL INDUKNYA");
+        System.out.println("\nTEK-KOTEK-KOTEK-KOTEK");
+        System.out.println("INDUK AYAM CARI ANAKNYA");
+        System.out.println("TEK-KOTEK-KOTEK-KOTEK");
+        System.out.println("AKHIRNYA, KETEMU SEMUA");
+        System.out.println("\nTEK-KOTEK-KOTEK-KOTEK");
+        System.out.println("INDUK AYAM CARI ANAKNYA");
+        System.out.println("TEK-KOTEK-KOTEK-KOTEK");
+        System.out.println("AKHIRNYA, KETEMU SEMUA");
+    
+        
+        System.out.print("\n\n");
+        } 
+    }
+
+
+// TERIMA KASIH - THANK YOU //
